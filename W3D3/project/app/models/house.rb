4e1f8bd05a6b5b6f793d84503ac42283b499bc0e
Natasha <wsyalways2@gmail.com>
2018-07-02 21:55:13 -0700

    complete W3D3 homework

---
+++ W3D3/project/app/models/house.rb
@@ -0,0 +1,9 @@
+class House < ApplicationRecord
+  validates :address, presence: true
+  has_many(
+    :people,
+    class_name: 'Person',
+    foreign_key: :house_id,
+    primary_key: :id
+  )
+end
