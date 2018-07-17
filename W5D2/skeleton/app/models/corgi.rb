0239c387a404139f34d47eda5ff0183454735d87
Natasha <wsyalways2@gmail.com>
2018-07-17 08:43:59 -0700

    finish w5d2 homework

---
+++ W5D2/skeleton/app/models/corgi.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: corgis
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Corgi < ApplicationRecord
+  include Toyable
+  validates :name, presence: true
+
+
+end
